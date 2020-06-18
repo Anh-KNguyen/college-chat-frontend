3bbb13a53bfd18ebee74280ee10883229de9975f
Anh Nguyen <anhknguyen97@yahoo.com>
2020-06-18 16:43:39 -0700

    Create post ui button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
+const postButton = <button>Post</button>
+
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    postButton,
     document.getElementById('root')
 );
