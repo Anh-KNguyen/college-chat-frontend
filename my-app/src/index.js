2c9fa5daaba7dc216c4865ceb7ffabf2364b507b
Anh Nguyen <anhknguyen97@yahoo.com>
2020-06-19 15:36:48 -0700

    Implement all onClick handlers

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,24 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-const MainPage = props => (
-    <div id="main-page-buttons">
-        
-    </div>
-)
-
 class App extends React.Component {
-    handleClick() {
-        console.log('Click happened');
+    handlePostClick() {
+        console.log('Post click happened');
+    }
+    handleDeleteClick() {
+        console.log('Delete click happened');
+    }
+    handleShowAllClick() {
+        console.log('Show All click happened');
+    }
+    handleShowSingleClick() {
+        console.log('Show Single click happened')
     }
     
     render() {
         return (
             <div>
-                <button id="post-button" onClick={this.handleClick}> Post </button>
-                <button id="delete-button"> Delete </button>
-                <button id="show-all-button"> Show All </button>
-                <button id="show-single-button"> Show Single</button>
+                <button id="post-button" onClick={this.handlePostClick}> Post </button>
+                <button id="delete-button" onClick={this.handleDeleteClick}> Delete </button>
+                <button id="show-all-button" onClick={this.handleShowAllClick}> Show All </button>
+                <button id="show-single-button" onClick={this.handleShowSingleClick}> Show Single</button>
             </div>
             
         );
